0b559a49a1f3722a481cc19b33396ebe768bcd03
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2024-12-28 18:37:54 +0530

    Update P1_a.py

--- Data Visulization with Python/P1_a.py
+++ Data Visulization with Python/P1_a.py
@@ -8,6 +8,6 @@ if m1 <= m2 and m1 <= m3:
     avgMarks = (m2 + m3) / 2
 elif m2 <= m1 and m2 <= m3:
     avgMarks = (m1 + m3) / 2
-elif m3 <= m1 and m2 <= m2:
+elif m3 <= m1 and m3 <= m2:
     avgMarks = (m1 + m2) / 2
 print("Average of best two test marks out of three test’s marks is", avgMarks)
