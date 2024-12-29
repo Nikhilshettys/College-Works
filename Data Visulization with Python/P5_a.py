25a3c04a147b2babd1d83ecd3bf2c6038747c2e2
HEMANTH S.P <142610966+Hassassin-10@users.noreply.github.com>
2024-12-29 15:23:23 +0530

    Update P5_a.py

--- Data Visulization with Python/P5_a.py
+++ Data Visulization with Python/P5_a.py
@@ -3,8 +3,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-data1 = np.random.normal(loc=-1, scale=1, size=1000)
-data2 = np.random.normal(loc=2, scale=1, size=1000)   
+data1 = np.random.randn(1000)
+data2 = np.random.normal(loc=3, scale=1, size=1000)   
 plt.hist([data1, data2], bins=30, stacked=True, color=['yellow', 'green'], edgecolor='black')
 plt.xlabel('Value')
 plt.ylabel('Frequency')
